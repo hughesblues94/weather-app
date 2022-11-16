00d6609cebb11beed2536ca46bfdf5db470ba210
hughesblues94 <hughesblues94@gmail.com>
2022-11-16 13:29:11 +0000

    added ForecastSummary test

---
+++ src/tests/Components/ForecastSummary.test.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import ForecastSummary from "../../components/ForecastSummary";
+
+describe("ForecastSummary", () => {
+    const validProps = {
+        date: 1111111,
+        description: "Stub description",
+        icon: "800",
+        temperature: {
+            min: 12,
+            max: 22,
+        },
+        onSelect: () => { },
+    };
+    it("renders correctly", () => {
+        const { asFragment } = render(
+            <ForecastSummary
+                date={validProps.date}
+                description={validProps.description}
+                icon={validProps.icon}
+                temperature={validProps.temperature}
+                onSelect={validProps.onSelect}
+            />
+        );
+
+        expect(asFragment()).toMatchSnapshot();
+    });
+});
